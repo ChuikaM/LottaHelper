d299141f4490afe4083afeea17d49ef9c70a02a8
ChuikaM <matveidoroshenko@gmail.com>
2026-02-08 00:09:19 +0300

    feat: add ability to get recommendations.

--- py/furniture.py
+++ py/furniture.py
@@ -1,10 +1,13 @@
+import json
 from openai import OpenAI
 from os import getenv
 
 OPENAI_API_KEY = getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-def get_furniture_description(image_url):
+def get_furniture_description(image_url: str, catalog_path = "../catalog/furniture.json") -> str:
+    with open(catalog_path, "r", encoding="utf-8") as f:
+        full_catalog = json.load(f)
     try:
         system_message = """
         You are an interior design analyzer. Your task:
@@ -14,50 +17,22 @@ def get_furniture_description(image_url):
         4. Respond only in the specified JSON format.
         5. If the image does not contain furniture, return empty JSON file.
         """
-        user_message = """
-        Furniture list from the interior design solution in JSON format.
-        Input: Image/PDF file of the interior design solution.
-        Output: [{"name":"chair","description":"white wood"]
-        """
         response = client.chat.completions.create(
-            model="gpt-4o-mini",
+            model="gpt-4o",
             messages=[
-                {"role": "system", "content": system_message},
+                {"role": "system", "content": system_message},     
                 {
                     "role": "user",
                     "content": [
-                        {"type": "text", "text": user_message},
+                        {"type": "text", "text": f"Каталог мебели: {full_catalog}. Analyze the image and select products that match the style. Return JSON with the fields name, price, match (%), and product_url."},
                         {"type": "image_url", "image_url": {"url": image_url}}
                     ]
                 }
             ],
-            max_tokens=500,
+            response_format={"type": "json_object"},
             temperature=0.1
         )   
     except Exception as e:
         return f"An error occurred: {e}"
     
     return response.choices[0].message.content.strip()    
-
-def match_furniture(json):
-    result = ""
-    return result
-
-
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
-
-def get_recommendations(json):
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    recommendations = ""
-
-    query = ""
-    item_text = ""
-
-    emb_query = model.encode([query])
-    emb_item = model.encode([item_text])
-
-    similarity = cosine_similarity(emb_query, emb_item)[0][0]
-    match_percent = round(similarity * 100, 1)
-
-    return recommendations
