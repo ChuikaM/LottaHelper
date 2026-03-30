3907d147891390e36202cc8b0c18a7dadde813da
ChuikaM <matveidoroshenko@gmail.com>
2026-03-30 20:52:52 +0300

    feat: update app/db/manager/redis_manager.py

--- app/db/manager/redis_manager.py
+++ app/db/manager/redis_manager.py
@@ -1,14 +1,19 @@
 import redis
+import os
 
 class RedisManager:
     def __init__(self):
+        redis_host = os.getenv('REDIS_HOST', 'redis')
+        redis_port = int(os.getenv('REDIS_PORT', 6379))
         self.__redis = redis.StrictRedis(
-            host='localhost', 
-            port=6379, 
-            db=0)
+            host=redis_host,
+            port=redis_port,
+            db=0,
+            decode_responses=True
+        )
     
     def add_token(self, token):
-        self.__redis.set('token', token)
+        self.__redis.setex(f"token:{token}", 3600, "valid")
     def remove_token(self, token):
         self.__redis.delete(token)
     def token_exists(self, token):
