ef6599d0d30164c8b764cf5bbdb450239dd925dd
ChuikaM <matveidoroshenko@gmail.com>
2026-04-05 15:50:31 +0300

    feat: update disk_manager.py

--- app/disk_manager.py
+++ app/disk_manager.py
@@ -7,6 +7,8 @@ import logging
 y = yadisk.YaDisk(token = os.getenv("YANDEX_DISK_TOKEN"))
 
 def upload_image_to_disk(base64_string : str, task_id):
+    if "," in base64_string:
+        base64_string = base64_string.split(",")[1]
     image_data = base64.b64decode(base64_string)
     file_object = io.BytesIO(image_data)
 
@@ -15,6 +17,5 @@ def upload_image_to_disk(base64_string : str, task_id):
         return None
     
     path_on_disk = f"/{task_id}.jpg"
-    y.upload(file_object, path_on_disk, overwrite=True)
-    direct_link = y.get_download_link(path_on_disk)
-    return direct_link
+    y.upload(file_object, path_on_disk)
+    return y.get_download_link(path_on_disk)
